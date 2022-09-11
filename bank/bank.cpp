25d7f45e69f5bef40a9d975fc1696621faee5880
Fr1xel <ivan.rados12354@gmail.com>
2022-09-11 19:53:58 +0200

    small projects

--- bank/bank.cpp
+++ bank/bank.cpp
@@ -1,20 +1,43 @@
 #include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
     const int sales = 95000;
-    const double stateTax = 4;
+    const int stateTax = 4;
     const int stateTaxMoney = sales * (stateTax / 100);
-    const double countyTax = 2;
+    const int countyTax = 2;
     const int countyTaxMoney = sales * (countyTax / 100);
-    const double allTax = stateTax + countyTax;
+    const int allTax = stateTax + countyTax;
     const int allTaxMoney = sales * (allTax / 100);
     cout << "Sales = $" << sales << endl
         << "State Tax = " << stateTax << "%" << " = " << stateTaxMoney << endl
         << "County Tax = " << countyTax << "%" << " = " << countyTaxMoney << endl
-        << "Total Tax = " << allTax << "%" << " = " << allTaxMoney;
+        << "Total Tax = " << allTax << "%" << " = " << allTaxMoney << endl;
+
+    cout << "Convert fahrenheit into celsius: ";
+    double fahrenheit;
+    cin >> fahrenheit;
+    const double celsious = (fahrenheit - 32) * (.5556);
+    cout << fahrenheit << " this many fahrenheit is equivelent to: " << celsious << " celsious" << endl;
+
+    const double pi = 3.14;
+    double radious {};
+    cout << "Enter a radious of your circle :";
+    cin >> radious;
+    double radiousSquared = pow(radious, 2);
+    double area = pi * radiousSquared;
+    cout << area << " is the area of your circle" << endl;
+
+    int minValue{ 1 };
+    int maxValue{ 6 };
+    srand(time(0));
+    int diceNumber{ (rand() % (maxValue - minValue)) + 1 };
+    cout << "You rolled the dice and got: " << diceNumber;
     return 0;
 }
   
